eb03453e77f590b6f48d830e0d6a437ad4490052
e^ln(VatteyHeng <tey0996@gmail.com>
2024-10-19 14:43:14 +0700

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,42 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServiceComponent } from './service/service.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  
+  {
+    path: '',
+    redirectTo:'home',
+    pathMatch:'full'
+    
+  },
+  {
+    path: 'home',
+    component:HomeComponent
+    
+    },
+    {
+    path: 'About',
+    component: AboutComponent
+    },
+    
+    
+    {
+    
+    path:'Contact',
+    component: ContactComponent
+    },
+    {
+    path: 'Service',
+    component: ServiceComponent
+    
+    
+    }
+
+
+
+
+];
---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { RouterEvent, RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [RouterLink,RouterLinkActive],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+
+
+
+
+
+
+
+
+
+
+  
+}
