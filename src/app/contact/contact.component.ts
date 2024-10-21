c1f2e5c21f17b8c1880b9c93f5eacc2b4ae3e3f5
e^ln(VatteyHeng <tey0996@gmail.com>
2024-10-21 11:14:05 +0700

    add

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css'], // Note: Corrected 'styleUrl' to 'styleUrls'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
 
+  ngOnInit() {
+    // Use jQuery once the component is initialized
+    this.initializeJQuery();
+    this.hi();
+
+  }
+
+  hi(){
+$(document).ready(function(){
+
+  $("p").css("color","red");
+  
+
+})
+
+console.log("hi");
+
+  }
+
+  initializeJQuery() {
+    $(document).ready(() => {
+      $("#hide").click(() => {
+        $("p").hide();
+      });
+      $("#show").click(() => {
+        $("p").show();
+      });
+    });
+
+
+  }
 }
